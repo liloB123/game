9f10c024a74db7f270f33ff269fa6e6745c6aedc
lihi <lihibloy327@gmail.com>
2023-09-18 16:50:04 +0300

    Merge remote-tracking branch 'origin/main'

--- screen.py
+++ screen.py
@@ -14,24 +14,30 @@ def green_board():
     green_board.fill(consts.GREEN_COLOR)
 
 
+
+
 def random_bushes_index():
     count = 0
 
     while count < 20:
-        bush_row_index = random.randint(0, 24)
-        bush_column_index = random.randint(0, 49)
-        bush_index = [bush_row_index, bush_column_index]
-        if bush_index not in bushes_index:
-            bushes_index.append(bush_index)
-            count += 1
+        row_index = random.randint(0, 24)
+        column_index = random.randint(0, 49)
+        place_bushes(row_index, column_index)
+
+        # game_field.create_board()[row_index][column_index] ==
+        # bush_index = [bush_row_index, bush_column_index]
+        # if bush_index not in bushes_index:
+        #     bushes_index.append(bush_index)
+        #     count += 1
 
 
-def place_bushes():
-    bush = pygame.image.load(consts.GRASS_IMG)
-    for bush_index in bushes_index:
-        bush_row = bush_index[0]
-        bush_column = bush_index[1]
-        game_field.create_board()[bush_row][bush_column] = bush
+def place_bushes(row_index, column_index):
+    game_field.create_board()[row_index][column_index] = pygame.image.load(consts.GRASS_IMG)
+    # bush = pygame.image.load(consts.GRASS_IMG)
+    # for bush_index in bushes_index:
+    #     bush_row = bush_index[0]
+    #     bush_column = bush_index[1]
+    #     game_field.create_board()[bush_row][bush_column] = bush
         # y_coordinate = bush_row * consts.SQUARE_SIZE
         # x_coordinate = bush_column * consts.SQUARE_SIZE
         # bush.blit(y_coordinate, x_coordinate)
@@ -39,6 +45,6 @@ def place_bushes():
 
 green_board()
 random_bushes_index()
-place_bushes()
+
 pygame.display.flip()  # can put at the end of the main
-pygame.time.wait(10000)
+# pygame.time.wait(10000)
