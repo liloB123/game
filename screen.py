d75268fbf99d5e94fa04841eff5d603f744245e2
liya <liyabiron@gmail.com>
2023-09-18 15:01:29 +0300

    update

--- screen.py
+++ screen.py
@@ -1,5 +1,6 @@
 import pygame
 import consts
+import random
 
 pygame.init() #should be in main
 
@@ -8,10 +9,24 @@ def green_board():
     pygame.display.set_caption("Game Of Life")
 
     green_board.fill(consts.GREEN_COLOR)
-    pygame.display.flip()
-
-def random_bushes():
 
 
+def random_bushes_index():
+    bushes_index = []
+    count = 0
 
+    while count < 20:
+        bush_row_index = random.randint(0, 24)
+        bush_column_index = random.randint(0, 49)
+        bush = [bush_row_index, bush_column_index]
+        if bush not in bushes_index:
+            bushes_index.append(bush)
+            count += 1
+
+
+
+
+
+
+# pygame.display.flip()  # can put at the end of the main
 # pygame.time.wait(10000)
