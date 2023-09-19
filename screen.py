c3e35dcd3a2340978a2d84ee44a8a00c05e813fe
liya <liyabiron@gmail.com>
2023-09-19 10:38:39 +0300

    update

--- screen.py
+++ screen.py
@@ -8,26 +8,22 @@ mines_index_list = []
 
 pygame.init() #should be in main
 
+
 green_board = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 pygame.display.set_caption("Game Of Life")
 green_board.fill(consts.GREEN_COLOR)
 
 
 
-def random_bushes_place(green_board):
+def random_bushes_place():
     count = 0
     while count < 20:
         grass_x = random.randint(0, (consts.BOARD_COLUMNS * consts.SQUARE_SIZE - consts.SQUARE_SIZE * 2))
         grass_y = random.randint(0, (consts.BOARD_ROWS * consts.SQUARE_SIZE - consts.SQUARE_SIZE * 2))
         green_board.blit(consts.GRASS_IMG, (grass_x, grass_y))
         count += 1
-def random_bushes_place():
-    count = 0
-    while count < 20:
-        grass_x = random.randint(0, (consts.BOARD_COLUMNS * consts.SQUARE_SIZE) - 30)
-        grass_y = random.randint(0, (consts.BOARD_ROWS * consts.SQUARE_SIZE) - 30)
-        green_board.blit(consts.GRASS_IMG, (grass_x, grass_y))
-        count += 1
+
+
 
 def put_soldier_on_screen():
     green_board.blit(consts.SOLDIER_IMG, (0,0))
@@ -35,6 +31,17 @@ def put_soldier_on_screen():
 def put_flag_on_screen():
     green_board.blit(consts.FLAG_IMG, (consts.WINDOW_WIDTH - 4 * consts.SQUARE_SIZE, consts.WINDOW_HEIGHT - 3 * consts.SQUARE_SIZE))
 
+def draw_text(message, font_size, color, location):
+    font = pygame.font.SysFont(consts.FONT_NAME, font_size)
+    text_img = font.render(message, True, color)
+    green_board.blit(text_img, location)
+
+
+def draw_start_message():
+    draw_text(consts.START_MESSAGE_1, consts.START_FONT_SIZE,
+                 consts.START_COLOR, consts.START_MESSAGE_1_LOCATION)
+    draw_text(consts.START_MESSAGE_2, consts.START_FONT_SIZE,
+              consts.START_COLOR, consts.START_MESSAGE_2_LOCATION)
 
 def black_board():
     black_board = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
@@ -56,17 +63,7 @@ def random_mines_place(mines_index_list, black_board):
         black_board.blit(consts.MINE_IMG, (mine_x, mine_y))
         count += 1
 
-def draw_text(message, font_size, color, location):
-    font = pygame.font.SysFont(consts.FONT_NAME, font_size)
-    text_img = font.render(message, True, color)
-    green_board.blit(text_img, location)
 
-
-def draw_start_message():
-    draw_text(consts.START_MESSAGE_1, consts.START_FONT_SIZE,
-                 consts.START_COLOR, consts.START_MESSAGE_1_LOCATION)
-    draw_text(consts.START_MESSAGE_2, consts.START_FONT_SIZE,
-              consts.START_COLOR, consts.START_MESSAGE_2_LOCATION)
 
 draw_start_message()
 put_soldier_on_screen()
