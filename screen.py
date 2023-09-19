a5df25356c89773bec12d567957f3faae488f52a
lihi <lihibloy327@gmail.com>
2023-09-19 10:31:38 +0300

    Merge remote-tracking branch 'origin/main'

--- screen.py
+++ screen.py
@@ -8,7 +8,7 @@ mines_index_list = []
 
 pygame.init() #should be in main
 
-green_board = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_LENGTH))
+green_board = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 pygame.display.set_caption("Game Of Life")
 green_board.fill(consts.GREEN_COLOR)
 
@@ -32,13 +32,8 @@ def put_soldier_on_screen():
     green_board.blit(consts.SOLDIER_IMG, (0,0))
 
 def put_flag_on_screen():
-    green_board.blit(consts.FLAG_IMG, (consts.WINDOW_WIDTH - 4 * consts.SQUARE_SIZE, consts.WINDOW_LENGTH - 3 * consts.SQUARE_SIZE))
+    green_board.blit(consts.FLAG_IMG, (consts.WINDOW_WIDTH - 4 * consts.SQUARE_SIZE, consts.WINDOW_HEIGHT - 3 * consts.SQUARE_SIZE))
 
-
-def draw_text(message, font_size, color, location):
-    font = pygame.font.SysFont(consts.FONT_NAME, font_size)
-    text_img = font.render(message, True, color)
-    green_board.blit(text_img, location)
 
 def black_board():
     black_board = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
@@ -60,7 +55,22 @@ def random_mines_place(mines_index_list, black_board):
         black_board.blit(consts.MINE_IMG, (mine_x, mine_y))
         count += 1
 
+def draw_text(message, font_size, color, location):
+    font = pygame.font.SysFont(consts.FONT_NAME, font_size)
+    text_img = font.render(message, True, color)
+    green_board.blit(text_img, location)
 
 
+def draw_start_message():
+    draw_text(consts.START_MESSAGE_1, consts.START_FONT_SIZE,
+                 consts.START_COLOR, consts.START_MESSAGE_1_LOCATION)
+    draw_text(consts.START_MESSAGE_2, consts.START_FONT_SIZE,
+              consts.START_COLOR, consts.START_MESSAGE_2_LOCATION)
+
+draw_start_message()
+put_soldier_on_screen()
+random_bushes_place()
+put_flag_on_screen()
 pygame.display.flip()  # can put at the end of the main
 pygame.time.wait(10000)
+
