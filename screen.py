4c7fb2ab313bed578bf14a97b2cd3923a1a2166d
lihi <lihibloy327@gmail.com>
2023-09-19 09:03:45 +0300

    Merge remote-tracking branch 'origin/main'

--- screen.py
+++ screen.py
@@ -16,35 +16,36 @@ def green_board():
 
 
 
-def random_bushes_index():
-    count = 0
 
-    while count < 20:
-        row_index = random.randint(0, 24)
-        column_index = random.randint(0, 49)
-        place_bushes(row_index, column_index)
-
-        # game_field.create_board()[row_index][column_index] ==
-        # bush_index = [bush_row_index, bush_column_index]
-        # if bush_index not in bushes_index:
-        #     bushes_index.append(bush_index)
-        #     count += 1
-
-
-def place_bushes(row_index, column_index):
-    game_field.create_board()[row_index][column_index] = pygame.image.load(consts.GRASS_IMG)
-    # bush = pygame.image.load(consts.GRASS_IMG)
-    # for bush_index in bushes_index:
-    #     bush_row = bush_index[0]
-    #     bush_column = bush_index[1]
-    #     game_field.create_board()[bush_row][bush_column] = bush
-        # y_coordinate = bush_row * consts.SQUARE_SIZE
-        # x_coordinate = bush_column * consts.SQUARE_SIZE
-        # bush.blit(y_coordinate, x_coordinate)
+# def random_bushes_index():
+#     count = 0
+#
+#     while count < 20:
+#         row_index = random.randint(0, 24)
+#         column_index = random.randint(0, 49)
+#         place_bushes(row_index, column_index)
+#
+#         # game_field.create_board()[row_index][column_index] ==
+#         # bush_index = [bush_row_index, bush_column_index]
+#         # if bush_index not in bushes_index:
+#         #     bushes_index.append(bush_index)
+#         #     count += 1
+#
+#
+# def place_bushes(row_index, column_index):
+#     game_field.create_board()[row_index][column_index] = pygame.image.load(consts.GRASS_IMG)
+#     # bush = pygame.image.load(consts.GRASS_IMG)
+#     # for bush_index in bushes_index:
+#     #     bush_row = bush_index[0]
+#     #     bush_column = bush_index[1]
+#     #     game_field.create_board()[bush_row][bush_column] = bush
+#         # y_coordinate = bush_row * consts.SQUARE_SIZE
+#         # x_coordinate = bush_column * consts.SQUARE_SIZE
+#         # bush.blit(y_coordinate, x_coordinate)
 
 
 green_board()
-random_bushes_index()
+# random_bushes_index()
 
 pygame.display.flip()  # can put at the end of the main
-# pygame.time.wait(10000)
+pygame.time.wait(10000)
