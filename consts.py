1f3cece03d215df31369b5c5b112b6f84f920625
lihi <lihibloy327@gmail.com>
2023-09-19 10:20:33 +0300

    Merge remote-tracking branch 'origin/main'

--- consts.py
+++ consts.py
@@ -20,3 +20,21 @@ LOSE_STATE = 1
 WIN_STATE = 3
 RUNNING_STATE = 2
 MINE_IMG = "mine.png"
+
+soldier_image = pygame.image.load("soldier.png")
+DEFAULT_SOLDIER_SIZE = (SQUARE_SIZE * 2, SQUARE_SIZE * 4)
+SOLDIER_IMG = pygame.transform.scale(soldier_image, DEFAULT_SOLDIER_SIZE)
+
+flag_image = pygame.image.load("flag.png")
+DEFAULT_FLAG_SIZE = (SQUARE_SIZE * 4, SQUARE_SIZE * 3)
+FLAG_IMG = pygame.transform.scale(flag_image, DEFAULT_FLAG_SIZE)
+
+FONT_NAME = "Calibri"
+START_MESSAGE_1 = "Welcome to The Flag game."
+START_MESSAGE_2 = "Have Fun!"
+START_FONT_SIZE = int(0.02 * WINDOW_WIDTH)
+START_COLOR = (255, 255, 255)
+START_MESSAGE_1_LOCATION = \
+    (0.05 * WINDOW_WIDTH, WINDOW_LENGTH / 18 - (START_FONT_SIZE / 2))
+START_MESSAGE_2_LOCATION = \
+    (0.05 * WINDOW_WIDTH, WINDOW_LENGTH / 10 - (START_FONT_SIZE / 2))
